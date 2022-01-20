1a237e4a33c4357df51cb6730a38c862f28c7a11
Humza23 <79542316+Humza23@users.noreply.github.com>
2022-01-20 16:49:55 -0500

    base

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import bkbanner from '../images/bkbanner.jpeg'
+
+const Header = () => {
+  return (
+    <div>
+        <img id='banner' src={bkbanner} alt="Brooklyn Nets"/>
+    </div>
+  
+  
+  );
+};
+
+export default Header;
