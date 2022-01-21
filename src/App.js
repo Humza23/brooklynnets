4890af7e44c0ccb7949c811097a0d62be5621ee0
Humza23 <79542316+Humza23@users.noreply.github.com>
2022-01-21 00:53:48 -0500

    roster pg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import Navbar from './components/Navbar';
@@ -8,8 +9,29 @@ import PlayerStats from './components/PlayerStats';
 import PlayerPages from './components/PlayerPages';
 import Injuries from './components/Injuries';
 import Schedule from './components/Schedule';
+import axios from 'axios'
+
+
 
 function App() {
+
+  const [rosterData, setRosterData] = useState([])
+  const rosterURL = "https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/2021/teams/nets_roster.json"
+
+  
+  useEffect(() => {
+      axios
+        .get(rosterURL)
+        .then((res) => {
+          setRosterData(res.data.t.pl);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+
+
+
   return (
     <div className="App">
       <Header/>
@@ -18,7 +40,7 @@ function App() {
     <Routes>
 
       <Route path='/' element={<HomeCarousel />}/>
-      <Route path='/roster' element={<Roster />}/>
+      <Route path='/roster' element={<Roster rosterData={rosterData} />} />
       <Route path='/playerstats' element={<PlayerStats />}/>
       <Route path='/playerpages' element={<PlayerPages />}/>
       <Route path='/schedule' element={<Schedule />}/>
--- src/components/Roster.js
+++ src/components/Roster.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Roster = () => {
+const Roster = (props) => {
+    const {rosterData} = props
+
   return(
-    <div>
-        <h1 style={{color: 'white'}}> hi </h1>
+    <div className='rosterGrid'>
+        {rosterData.map((player, idx) => (
+            <div className='card border-dark mb 3' key={idx} >
+            <img className="rosterImg" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.pid}.png`} alt={player.fn} style={{width:'18rem', height: '10rem'}}/>
+               <p className='rosterGridName'>
+                {player.fn} {player.ln}
+                </p>   
+                <p className='rosterGridNum'>
+                #{player.num}
+                </p>
+            </div>
+        ))
+        }
+        
     </div>
   );
 };
